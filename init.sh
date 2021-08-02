f593493431ebb5aed6a26b7860f998746343e6de
Evgeniy Chu <chuevgenii@gmail.com>
2021-08-02 15:53:50 +0300

    work

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+# sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/mysql start
+# sudo gunicorn --bind='0.0.0.0:8080' hello:test
