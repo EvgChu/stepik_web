297ac3bdfbd0409a95c76516f54b25b5949cd903
Evgeniy Chu <chuevgenii@gmail.com>
2021-08-03 11:12:54 +0300

    task 1.8 (2)

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
-sudo gunicorn --bind='0.0.0.0:8080' hello:simple_app 
+sudo gunicorn --bind='0.0.0.0:8080' hello:simple_app &
+curl -vv '127.0.0.1:8080/?a=1&a=2&b=3'
+curl -vv '127.0.0.1:8080/?a=1&a=2&b=3'
