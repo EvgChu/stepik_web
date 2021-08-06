9a4d0a5839f1790c144e10a57e96efe6ad0817b6
Evgeniy Chu <chuevgenii@gmail.com>
2021-08-06 09:47:14 +0300

    for st

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 
-
+def just_function(first,second):
+    return
 
 def simple_app(environ, start_response):
     """
@@ -10,4 +11,8 @@ def simple_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return environ.split("&")
+    return "\n".join(environ.strip("/?").split("&"))
+
+if __name__ == "__main__":
+    print("For test")
+    print(simple_app("/?a=1&b=2",just_function))
--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ sudo /etc/init.d/nginx restart
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
 sudo gunicorn --bind='0.0.0.0:8080' hello:simple_app &
-curl -vv '127.0.0.1:8080/?a=1&a=2&b=3'
-curl -vv '127.0.0.1:8080/?a=1&a=2&b=3'
+curl -vv 'httpT://127.0.0.1/'
+curl -vv 'httpT://127.0.0.1/hello/?a=1&a=2&b=3'
+curl -vv 'httpT://127.0.0.1:8080/?a=1&a=2&b=3'
