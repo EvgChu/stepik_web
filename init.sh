d6733cbbcb90fff6ab530366392d562b372cd2d4
KinChu <chuevgenii@gmail.com>
2022-01-14 19:58:26 +0300

    added update cmds

--- init.sh
+++ init.sh
@@ -2,10 +2,28 @@ echo "Start nginx"
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo cp -f etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-printenv VIRTUAL_ENV
+
 echo "Start gunicorn" 
-sudo ./venv/bin/gunicorn -c etc/conf_gunicorn_hello.py hello:simple_app &
-sudo ./venv/bin/gunicorn -c etc/conf_gunicorn_dj.py ask.wsgi:application &
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+    echo "VIRTUAL_ENV" 
+    sudo ./venv/bin/gunicorn -c etc/conf_gunicorn_hello.py hello:simple_app &
+    sudo ./venv/bin/gunicorn -c etc/conf_gunicorn_dj.py ask.wsgi:application &
+else
+    echo "not VIRTUAL_ENV" 
+    sudo apt-get update
+    sudo apt-get install -y python3.5
+    sudo apt-get install -y python3.5-dev
+    sudo unlink /usr/bin/python3
+    sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+    sudo pip3 install --upgrade pip
+    sudo pip3 install --upgrade django==2.1
+    sudo pip3 install --upgrade gunicorn
+
+    sudo gunicorn -c etc/conf_gunicorn_hello.py hello:simple_app &
+    sudo gunicorn -c etc/conf_gunicorn_dj.py ask.wsgi:application &
+fi
+
 
 # sleep 1
 # curl -vv 'http://127.0.0.1/'
