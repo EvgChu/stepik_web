c1e60b82d9e5286a7d3fb48e79598c910256f7b8
Evg Chu <chuevgenii@gmail.com>
2022-02-13 01:02:12 +0300

    cope models from other student

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,38 +5,29 @@ from django.contrib.auth.models import User
 
 
 class QuestionManager(models.Manager):
-  def new(self):
-    return self.order_by('-added_at')
-  def popular(self):
-    return self.order_by('-rating')
+    def new(self):
+        return super(QuestionManager, self).get_queryset().order_by('-id')
+    def popular(self):
+        return super(QuestionManager, self).get_queryset().annotate(models.Count('likes')).order_by('likes__count')
 
 
 class Question(models.Model):
-    # Question - 
-    objects = QuestionManager() 
-    # title - 
-    title = models.CharField(max_length=255)
-    # text - 
-    text = models.TextField()
-    # added_at - 
-    added_at = models.DateTimeField(blank=True, auto_now_add=True) 
-    # rating - 
-    rating = models.IntegerField()
-    # author - 
+    title = models.CharField(max_length=255, default='')
+    text = models.TextField(max_length=255, default='')
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(null=True)
+
     author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    # likes - 
     likes = models.ManyToManyField(User, related_name='question_like_user')
-    
-    
+
+
+    objects = QuestionManager()
+
 class Answer(models.Model):
-    # Answer - 
-    # text - 
-    text = models.TextField()
-    # added_at - 
-    added_at = models.DateTimeField(blank=True, auto_now_add=True) 
-    # question - 
+    text = models.TextField(max_length=255, default='')
+    added_at = models.DateTimeField(auto_now_add=True)
+
     question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
-    # author - 
-    aauthor = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
     
